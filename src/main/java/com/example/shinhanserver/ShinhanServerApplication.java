58f7514c8899d437ff616fbfae929bbac4d49c0e
younghyun-na <younghyunna@naver.com>
2023-11-14 15:48:52 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/example/shinhanserver/ShinhanServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.shinhanserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinhanServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShinhanServerApplication.class, args);
+  }
+
+}
