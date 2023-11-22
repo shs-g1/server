faabeca14fbe3b5b9c0dbe93386f9c42f9830e99
BoyoungH <hyong0903@naver.com>
2023-11-22 12:42:36 +0900

    chore : clientinfo 데이터 변경

--- src/main/java/com/example/shinhanserver/domain/form/FormService.java
+++ src/main/java/com/example/shinhanserver/domain/form/FormService.java
@@ -12,116 +12,6 @@ import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 
-//@Slf4j
-//@Service
-//@AllArgsConstructor
-//public class FormService {
-//
-//    private final CertificationRepository certificationRepository;
-//    private final CareerRepository careerRepository;
-//    private final EducationRepository educationRepository;
-//    private final PBRepository pbRepository;
-//    private final SpecializationRepository specializationRepository;
-//
-//    @Transactional
-//    public FormDto.FormResponseDto saveForm(FormDto.FormRequestDto formRequestDto) {
-//        // PB 저장
-//        PB pb = savePB(formRequestDto);
-//
-//        // Specialization 저장
-//        saveSpecializations(formRequestDto.getSpecializationList(), pb);
-//
-//        // Certification 저장
-//        saveCertifications(formRequestDto.getCertificateList(), pb);
-//
-//        // Career 저장
-//        saveCareers(formRequestDto.getCareerList(), pb);
-//
-//        // Education 저장
-//        saveEducations(formRequestDto.getEducationList(), pb);
-//
-//
-//        // 필요에 따라 추가적인 로직 수행
-//
-//        return FormDto.FormResponseDto.builder()
-//                .formId(pb.getId())
-//                .message("Form saved successfully")
-//                .build();
-//    }
-//
-//    private PB savePB(FormDto.FormRequestDto formRequestDto) {
-//
-//        // formRequestDto에서 필요한 정보를 추출하여 PB 엔터티에 저장
-//        PB pb = PB.builder()
-//                .name(formRequestDto.getName())
-//                .phone(formRequestDto.getPhoneNumber())
-//                .email(formRequestDto.getEmail())
-//                .image(formRequestDto.getImageUrl())
-//                .introduction(formRequestDto.getIntroduction())
-//                .build();
-//
-//
-//        // PB 엔터티를 저장
-//        return pbRepository.save(pb);
-//    }
-//
-//    private void saveCertifications(List<FormDto.CertificationDto> certificationDtos, PB pb) {
-//        // CertificationDto 리스트를 Certification 엔터티로 변환하여 저장
-//        for (FormDto.CertificationDto certificationDto : certificationDtos) {
-//            Certification certification = Certification.builder()
-//                    .name(certificationDto.getName())
-//                    .publisher(certificationDto.getPublisher())
-//                    .date(certificationDto.getDate())
-//                    .pb(pb)
-//                    .build();
-//
-//            certificationRepository.save(certification);
-//        }
-//    }
-//
-//    private void saveCareers(List<FormDto.CareerDto> careerDtos, PB pb) {
-//        // CareerDto 리스트를 Career 엔터티로 변환하여 저장
-//        for (FormDto.CareerDto careerDto : careerDtos) {
-//            Career career = Career.builder()
-//                    .organization(careerDto.getCompany())
-//                    .position(careerDto.getDepartment())
-//                    .start_date(careerDto.getStartDate())
-//                    .end_date(careerDto.getEndDate())
-//                    .pb(pb)
-//                    .build();
-//
-//            careerRepository.save(career);
-//        }
-//    }
-//
-//    private void saveEducations(List<FormDto.EducationDto> educationDtos, PB pb) {
-//        // EducationDto 리스트를 Education 엔터티로 변환하여 저장
-//        for (FormDto.EducationDto educationDto : educationDtos) {
-//            Education education = Education.builder()
-//                    .edu(educationDto.getSchool())
-//                    .major(educationDto.getDepartment())
-//                    .start_date(educationDto.getStartDate())
-//                    .end_date(educationDto.getEndDate())
-//                    .pb(pb)
-//                    .build();
-//
-//            educationRepository.save(education);
-//        }
-//    }
-//
-//    private void saveSpecializations(List<String> specializationDtos, PB pb) {
-//        // SpecializationDto 리스트를 Specialization 엔터티로 변환하여 저장
-//        for (String specializationDto : specializationDtos) {
-//            Specialization specialization = Specialization.builder()
-//                    .field(specializationDto)
-//                    .pb(pb)
-//                    .build();
-//
-//            specializationRepository.save(specialization);
-//        }
-//    }
-//}
-
 @Slf4j
 @Service
 @AllArgsConstructor
@@ -160,8 +50,6 @@ public class FormService {
         // Education 저장
         saveEducations(formRequestDto.getEducationList(), pb);
 
-        // 필요에 따라 추가적인 로직 수행
-
         return FormDto.FormResponseDto.builder()
                 .formId(pb.getId())
                 .message("Form saved successfully")
@@ -175,8 +63,6 @@ public class FormService {
         pb.setEmail(formRequestDto.getEmail());
         pb.setImage(formRequestDto.getImageUrl());
         pb.setIntroduction(formRequestDto.getIntroduction());
-        // 기타 필요한 필드들을 업데이트
-
         // PB 저장
         pbRepository.save(pb);
     }
